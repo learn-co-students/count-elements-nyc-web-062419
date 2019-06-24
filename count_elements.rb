53c8b5f8c5b22dac372caedea920fa5eccec7755
adriennem76 <adrienne.c.miranda@gmail.com>
2019-06-24 18:04:06 -0400

    element count

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  counted_elements = {}
+  array.each do |element|
+    if counted_elements.keys.include?(element)
+      counted_elements[element] += 1
+    else
+      counted_elements[element] = 1
+    end
+  end
+  counted_elements
 end
  
