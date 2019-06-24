5b870cf00f4b80095bd70f218f2730847cdd84b4
Anthony Wan <Anthonywan00@gmail.com>
2019-06-24 17:42:11 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+require "pry"
+
 def count_elements(array)
   # code goes here
+  hash = {}
+  array.map do |animal|
+    if hash[animal]
+      hash[animal] += 1
+    else 
+      hash[animal] = 1
+    end
+  end
+    hash
 end
  
